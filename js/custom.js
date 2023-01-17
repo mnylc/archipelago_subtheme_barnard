26021a7b3dc9f8a92d6bef4fc891303f1694e4f6
Diego Pino Navarro <dpino@metro.org>
2023-01-17 11:07:51 -0300

    make it once, smarter, so we don't end with many views-display-links

--- js/custom.js
+++ js/custom.js
@@ -8,10 +8,13 @@
   'use strict';
 
   Drupal.behaviors.archipelago_subtheme_barnard = {
-    attach: function(context, settings) {
-      $(".view-header .views-display-link").detach().appendTo("#main-breadcrumbs");
-
+    attach: function (context, settings) {
+      if ($(context).is('.view') || context == document) {
+          $("#main-breadcrumbs").find('.views-display-link').remove();
+          $(context).once('view-header-barnard').find('.view-header .views-display-link').each(function () {
+            $(this).detach().appendTo("#main-breadcrumbs");
+        });
+      }
     }
-  };
-
+  }
 })(jQuery, Drupal);
