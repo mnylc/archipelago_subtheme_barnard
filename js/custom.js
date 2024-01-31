53df9d40708cb2f55238a295bd283d8ada805ea2
Diego Pino Navarro <DiegoPino@users.noreply.github.com>
2024-01-31 18:33:04 -0500

    alternate (for collections too)

--- js/custom.js
+++ js/custom.js
@@ -10,15 +10,17 @@
   Drupal.behaviors.archipelago_subtheme_barnard = {
     attach: function (context, settings) {
       if ($(context).is('.view') || context == document) {
-         const $narrowsearch_block = document.querySelector('#block-narrowyoursearch');
-         if ($narrowsearch_block) {
-           const $facets = $narrowsearch_block.parentElement.querySelectorAll('.block-facets:not(.hidden)');
-           if ($facets.length > 0) {
-             $narrowsearch_block.classList.remove('visually-hidden')
-           }
-           else {
-             $narrowsearch_block.classList.add('visually-hidden')
-           }
+         const $narrowsearch_block = document.querySelectorAll('#block-narrowyoursearch, #block-narrowyoursearch-2');
+         if ($narrowsearch_block.length > 0) {
+           [].forEach.call($narrowsearch_block, function (el) {
+             const $facets = $narrowsearch_block.parentElement.querySelectorAll('.block-facets:not(.hidden)');
+             if ($facets.length > 0) {
+               el.classList.remove('visually-hidden')
+             }
+             else {
+               el.classList.add('visually-hidden')
+             }
+           });
          }
           $("#main-breadcrumbs").find('.views-display-link').remove();
           const elementsToAttach = once('view-header-barnard', '.view-header .views-display-link', context);
