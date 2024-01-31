b9071dd7be6bb49742e46bfbf125366f53581db5
Diego Pino Navarro <DiegoPino@users.noreply.github.com>
2024-01-31 18:24:15 -0500

    try hiding narrow your search IF there are no facets

--- js/custom.js
+++ js/custom.js
@@ -10,6 +10,16 @@
   Drupal.behaviors.archipelago_subtheme_barnard = {
     attach: function (context, settings) {
       if ($(context).is('.view') || context == document) {
+         const $narrowsearch_block = document.querySelector('#block-narrowyoursearch');
+         if ($narrowsearch_block) {
+           const $facets = $narrowsearch_block.parentElement.querySelectorAll('.block-facets');
+           if ($facets.length > 0) {
+             $narrowsearch_block.classList.remove('visually-hidden')
+           }
+           else {
+             $narrowsearch_block.classList.add('visually-hidden')
+           }
+         }
           $("#main-breadcrumbs").find('.views-display-link').remove();
           const elementsToAttach = once('view-header-barnard', '.view-header .views-display-link', context);
           $(elementsToAttach).each(function (index, value) {
